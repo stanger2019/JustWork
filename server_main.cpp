f38055f69deac5d1c0caa88e2bd21e023f98cb4e
stanger2019 <alekseew.yaroslaw2006@gmail.com>
2022-09-25 17:03:53 +0300

    add lib

--- server_main.cpp
+++ server_main.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
-int main(){
-    while(true){
-    std::cout<<"Hellow World";
-    }
-} 
+#include <all.hpp>
+int main()
+{
+    restinio::run(
+        restinio::on_this_thread()
+        .port(8080)
+        .address("localhost")
+        .request_handler([](auto req) {
+            return req->create_response().set_body("Hello, World!").done();
+        }));
+    return 0;
+}
